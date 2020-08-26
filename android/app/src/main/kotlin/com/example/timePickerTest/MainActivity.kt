f0ea501b1a24da5803033ba6f6fa80223e44567b
sultanularefin <mhmdarefin@gmail.com>
2020-08-27 04:56:29 +0600

    expecting a top level declartion error in mainActivity.kt

--- android/app/src/main/kotlin/com/example/timePickerTest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/timePickerTest/MainActivity.kt
@@ -1,58 +1,92 @@
 package com.example.timePickerTest
-//import android.bluetooth.BluetoothDevice
-//import android.content.BroadcastReceiver
-//import android.content.Context
-//import android.content.Intent
-//import android.content.IntentFilter
-//import android.os.Bundle
-//import android.widget.Toast
-//import androidx.appcompat.app.AppCompatActivity
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+
+
+/*
+1st iteration..
+package com.example.timePickerTest
+ */
+
+
+/*
+// 1st iternation...
 import io.flutter.embedding.android.FlutterActivity
 
 
 class MainActivity: FlutterActivity() {
-    
+
 }
 
+*/
 
 
 
-/*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title = "KotlinApp"
-        val filter = IntentFilter()
-        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED)
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
-        this.registerReceiver(broadcastReceiver, filter)
-    }
-    private val broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-        var device: BluetoothDevice? = null
-        override fun onReceive(
-                context: Context,
-                intent: Intent
-        ) {
-            val action = intent.action
-            device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-            if (BluetoothDevice.ACTION_ACL_CONNECTED == action) {
-                Toast.makeText(
-                        baseContext,
-                        "Device is now Connected",
-                        Toast.LENGTH_SHORT
-                ).show()
-            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED == action) {
-                Toast.makeText(
-                        baseContext,
-                        "Device is disconnected",
-                        Toast.LENGTH_SHORT
-                ).show()
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+
+
+
+
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "samples.flutter.dev/battery"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        /*
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            // Note: this method is invoked on the main thread.
+            // TODO
+        }
+
+        */
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // Note: this method is invoked on the main thread.
+            call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
             }
         }
     }
+
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+
+
+
+
 }
-*/
+
